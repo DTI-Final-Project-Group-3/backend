1d3f6dd2fcb4acd6b121f0fad8cd856ba503db76
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2025-01-25 16:19:49 +0700

    chore: test commit

---
+++ src/main/java/com/warehub/warehub/infrastructure/product/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.warehub.warehub.infrastructure.product.controller;
+
+public class ProductController {
+}
---
+++ src/main/java/com/warehub/warehub/infrastructure/product/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.warehub.warehub.infrastructure.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository {
+}
