316df994712e9704aefc8b3cccfaee764b0b30ac
mdedealf <dede.alfaruqq@gmail.com>
2025-02-23 22:47:17 +0700

    chore : delete duplicate warehouse admin repository

--- src/main/java/com/warehub/warehub/infrastructure/warehouse/repository/WarehouseAdminRepository.java
+++ src/main/java/com/warehub/warehub/infrastructure/warehouse/repository/WarehouseAdminRepository.java
@@ -4,10 +4,12 @@ import com.warehub.warehub.entity.Role;
 import com.warehub.warehub.entity.User;
 import com.warehub.warehub.entity.WarehouseAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface WarehouseAdminRepository extends JpaRepository<WarehouseAdmin, Long> {
     Optional<WarehouseAdmin> findByUserAssigneeId(Long userId);
     List<WarehouseAdmin> findByWarehouseId(Long warehouseId);
