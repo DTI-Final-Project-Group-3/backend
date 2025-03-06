03a25735d22d379dbbe94fcccde1c9ee2e7f9f0d
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2025-03-06 23:29:36 +0700

    refactor account and warehouse management code and optimizing warehous e assignment fetch code

--- src/main/java/com/warehub/warehub/infrastructure/warehouse/repository/WarehouseAdminRepository.java
+++ src/main/java/com/warehub/warehub/infrastructure/warehouse/repository/WarehouseAdminRepository.java
@@ -2,8 +2,10 @@ package com.warehub.warehub.infrastructure.warehouse.repository;
 
 import com.warehub.warehub.entity.Role;
 import com.warehub.warehub.entity.User;
+import com.warehub.warehub.entity.Warehouse;
 import com.warehub.warehub.entity.WarehouseAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,11 @@ import java.util.Optional;
 public interface WarehouseAdminRepository extends JpaRepository<WarehouseAdmin, Long> {
     Optional<WarehouseAdmin> findByUserAssigneeId(Long userId);
     List<WarehouseAdmin> findByWarehouseId(Long warehouseId);
+
+    @Query("""
+    SELECT wa FROM WarehouseAdmin wa
+    JOIN FETCH wa.warehouse w
+    WHERE w.deletedAt IS NULL
+    """)
+    List<WarehouseAdmin> findAllWarehouseAdminsWithWarehouse();
 }
