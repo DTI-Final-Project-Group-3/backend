2f0e8b68b8527daea9168fbbda69620ffb254033
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2025-03-09 14:01:38 +0700

    feat: add paginated product category api

--- src/main/java/com/warehub/warehub/infrastructure/product/repository/ProductCategoryRepository.java
+++ src/main/java/com/warehub/warehub/infrastructure/product/repository/ProductCategoryRepository.java
@@ -1,7 +1,11 @@
 package com.warehub.warehub.infrastructure.product.repository;
 
 import com.warehub.warehub.entity.ProductCategory;
+import com.warehub.warehub.infrastructure.product.dto.ProductCategoryResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +17,13 @@ public interface ProductCategoryRepository extends JpaRepository<ProductCategory
     Optional<ProductCategory> findByIdAndDeletedAtIsNull(Long id);
 
     List<ProductCategory> findAllByDeletedAtIsNull();
+
+    @Query(value = """
+            SELECT
+                pc.id,
+                pc.name
+            FROM product_categories pc
+            WHERE pc.deleted_at IS NULL
+            """, nativeQuery = true)
+    Page<ProductCategoryResponseDTO> findPaginatedProductCategories(Pageable pageable);
 }
